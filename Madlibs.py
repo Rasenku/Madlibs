84cede3a5adca599d574fa37c73c20f8f127694e
Dahveyea <dahveyea.cowan@gmail.com>
2019-09-01 12:05:27 -0700

    Fixed Indentation error

--- Madlibs.py
+++ Madlibs.py
@@ -1,3 +1,4 @@
+
 Verb = input("Enter a Verb:")
 Noun = input("Enter a Noun:")
 Verb = input("Enter a Verb:")
@@ -8,15 +9,13 @@ Verb = input("Enter a Verb")
 
 
 
-
-
 def show():
-print("You wake up " + Verb + " Staring at the wall")
-print("Then a " + Noun + " comes and blows the roof of the house that you are in")
-print("You " + Verb + " and now you are in space")
-print(Adverb + " you take a deep breath and you are in a war zone")
-print("You try ask yourself what's going on then you get shot in the " + Noun)
-print("All of a sudden " + Pronoun + " in a jungle with a broken leg")
-print("Then you decide to " + Verb + " full into a tiger and when you got close you woke up in your bed with chains on")
 
+    print("You wake up" + Verb + " Staring at the wall")
+    print("Then a " + Noun + " comes and blows the roof of the house that you are in")
+    print("You " + Verb + " and now you are in space")
+    print(Adverb + " you take a deep breath and you are in a war zone")
+    print("You try ask yourself what's going on then you get shot in the " + Noun)
+    print("All of a sudden " + Pronoun + " in a jungle with a broken leg")
+    print("Then you decide to " + Verb + " full into a tiger and when you got close you woke up in your bed with chains on")
 show()
