efd84a436d818c54644af65641c240321d5b4137
Dahveyea <dahveyea.cowan@gmail.com>
2019-08-31 22:04:55 -0700

    wrote and called functions

--- Madlibs.py
+++ Madlibs.py
@@ -1,4 +1,3 @@
-
 Verb = input("Enter a Verb:")
 Noun = input("Enter a Noun:")
 Verb = input("Enter a Verb:")
@@ -8,10 +7,16 @@ Pronoun = input("Enter a Pronoun:")
 Verb = input("Enter a Verb")
 
 
-print("You wake up" + Verb + " Staring at the wall")
-print("Then a" + Noun + "comes and blows the roof of the house that you are in")
-print("You" + Verb + "and now you are in space")
-print(Adverb + "you take a deep breath and you are in a war zone")
-print("You try ask yourself what's going on then you get shot in the" + Noun)
-print("All of a sudden" + Pronoun + "in a jungle with a broken leg")
-print("Then you decide to" + Verb + " full into a tiger and when you got close you woke up in your bed with chains on")
+
+
+
+def show():
+print("You wake up " + Verb + " Staring at the wall")
+print("Then a " + Noun + " comes and blows the roof of the house that you are in")
+print("You " + Verb + " and now you are in space")
+print(Adverb + " you take a deep breath and you are in a war zone")
+print("You try ask yourself what's going on then you get shot in the " + Noun)
+print("All of a sudden " + Pronoun + " in a jungle with a broken leg")
+print("Then you decide to " + Verb + " full into a tiger and when you got close you woke up in your bed with chains on")
+
+show()
